1b932a5c0de8a55ce0919bd7734ae67e75bbfbb5
zahiir <zahiirchoychoo@gmail.com>
2025-04-14 11:30:36 +0400

    Design of Academics page

--- Courses/views.py
+++ Courses/views.py
@@ -3,8 +3,11 @@ from Courses.models import Course
 # Create your views here.
 
 def home(request):
+   return render(request, 'courses/home.html')
+
+def academics(request):
    courses = Course.objects.all()
-   return render(request, 'courses/home.html', {'courses': courses})
+   return render(request, 'courses/academics.html', {'courses': courses})
 
 
 
